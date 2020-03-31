34cc49ac1a818e11c2ac3186332936440581a964
ujjwal <you@example.com>
2020-04-01 00:50:02 +0530

    new weather option added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (response.body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
+            callback(undefined, response.body.daily.data[0].summary + ' It is currently ' + response.body.currently.temperature + ' degress out. Temperature Max: ' + response.body.daily.data[0].temperatureMax + ', Temperature Min: ' + response.body.daily.data[0].temperatureMin+ '. There is a ' + response.body.currently.precipProbability + '% chance of rain.')
         }
     })
 }
